0b9d083fabd4df21559f4246f3f7bb92c4181fcc
Vladyslav Vynnytskyi <vynnytskyivladyslav@gmail.com>
2026-05-13 14:29:25 +0200

    Implement Layer class with forward pass and random initialization

--- include/layer.h
+++ include/layer.h
@@ -1,4 +1,12 @@
 #pragma once
+#include "matrix.h"
 
 class Layer {
+public:
+    Matrix weights;
+    Matrix biases;
+
+    Layer(int inputSize, int outputSize);
+
+    Matrix forward(const Matrix& input) const;
 };
--- src/layer.cpp
+++ src/layer.cpp
@@ -1,1 +1,12 @@
 #include "layer.h"
+
+Layer::Layer(int inputSize, int outputSize) 
+    : weights(outputSize, inputSize), biases(outputSize, 1) {
+    weights.randomize();
+    biases.randomize();
+}
+
+Matrix Layer::forward(const Matrix& input) const {
+    Matrix result = weights.multiply(input);
+    return result.add(biases);
+}
